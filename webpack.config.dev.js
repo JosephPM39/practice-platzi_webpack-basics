fbe288a8589923a7299f5e2daba0c7f89aa3a47a
JosephPM39 <josias.jjpm49@hotmail.com>
2022-03-05 17:47:18 -0600

    Configurando Webpack-analyzer para analizar los bundle

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     // Entry nos permite decir el punto de entrada de nuestra aplicación
@@ -67,7 +68,8 @@ module.exports = {
                 }
             ]
         }),
-        new Dotenv()
+        new Dotenv(),
+        new BundleAnalyzerPlugin()
     ],
     devServer: {
         static: path.join(__dirname, 'dist'),
