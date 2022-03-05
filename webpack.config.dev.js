4d727d44768b5dbdf24782d8513b4bcce01e9bea
JosephPM39 <josias.jjpm49@hotmail.com>
2022-03-05 17:28:03 -0600

    Configurando servidor de desarrollo en webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -68,5 +68,11 @@ module.exports = {
             ]
         }),
         new Dotenv()
-    ]
+    ],
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true,
+        port: 3005
+    }
 }
