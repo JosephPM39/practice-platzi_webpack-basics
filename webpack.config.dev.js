7495b0437da6495b4f8d23130989a91425cc4dea
JosephPM39 <josias.jjpm49@hotmail.com>
2022-03-06 08:31:58 -0600

    Instalando y configurando webpack con react

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,81 +1,29 @@
+const { resolve } = require('path');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    // Entry nos permite decir el punto de entrada de nuestra aplicación
-    entry: "./src/index.js",
-    // Output nos permite decir hacia dónde va enviar lo que va a preparar webpacks
-    output: {
-        // path es donde estará la carpeta donde se guardará los archivos
-        // Con path.resolve podemos decir dónde va estar la carpeta y la ubicación del mismo
-        path: path.resolve(__dirname, "dist"),
-        // filename le pone el nombre al archivo final
-        filename: "[name].[contenthash].js",
-        assetModuleFilename: 'assets/images/[hash][ext][query]'
-    },
-    mode: 'development',
-    devtool: 'source-map',
-    resolve: {
-        // Aqui ponemos las extensiones que tendremos en nuestro proyecto para webpack los lea
-        extensions: [".js"],
-        alias: {
-            '@utils': path.resolve(__dirname, 'src/utils/'),
-            '@templates': path.resolve(__dirname, 'src/templates/'),
-            '@styles': path.resolve(__dirname, 'src/styles/'),
-            '@images': path.resolve(__dirname, 'src/assets/images/'),
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         }
-
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css|.styl$/i,
-                use: [MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'stylus-loader'
-                ],
-            },
-            {
-                test: /\.(jpe?g|svg|png|gif|ico|eot|ttf|woff2?)(\?v=\d+\.\d+\.\d+)?$/i,
-                type: 'asset/resource',
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            inject: true,
-            template: './public/index.html',
-            filename: './index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'assets/[name].[contenthash].css'
-        }),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, 'src', 'assets/images'),
-                    to: 'assets/images'
-                }
-            ]
-        }),
-        new Dotenv(),
-        new BundleAnalyzerPlugin()
-    ],
-    devServer: {
-        static: path.join(__dirname, 'dist'),
-        compress: true,
-        historyApiFallback: true,
-        port: 3005
-    }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3006
+  }
 }
